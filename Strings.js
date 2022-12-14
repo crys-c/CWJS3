2a19c864982b766465e03fddc696d8590a2faa1c
crys-c <86004961+crys-c@users.noreply.github.com>
2022-12-13 17:17:05 -0800

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,19 @@
+//Task
+//misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+//When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n",o1="O", o2="o"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){ 
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){ 
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
